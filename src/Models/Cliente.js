4325e95040fc0abfcb9da21c7fb6be4be87f4ac8
Luiz Cláudio <lzcdoss@gmail.com>
2021-06-11 22:33:35 -0300

    Create Cliente.js

---
+++ src/Models/Cliente.js
@@ -0,0 +1,9 @@
+
+module.exports = class Cliente {
+    constructor(nome, idade, email) {
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+        this.dataCadastro = new Date();
+    }
+}
