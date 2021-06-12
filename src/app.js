e4f47f6463ac371ec6b4fd04d145951b13df4d4b
Luiz Cláudio <lzcdoss@gmail.com>
2021-06-11 23:53:40 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,16 +8,33 @@ app.use(express.urlencoded({ extended: true }))
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
-const lstTestes = [
+const allEvents = [
   {
-    name: 'teste1',
-    data: new Date()
+    name: 'Express',
+    link: 'https://expressjs.com/pt-br/'
+  },
+  {
+    name: 'Pug',
+    link: 'https://pugjs.org/api/getting-started.html'
   }
 ]
+
+app.get('/start', (req, res) => res.json(allEvents))
+
+app.post('/post-test', (req, res) => {
+  console.log('body:', req.body);
+  let cliente1 = new Cliente(req.body.nome, req.body.idade, req.body.email);
+  res.sendStatus(200).json(cliente1);
+  console.table(cliente1);
+});
+
+const lstTestes = [{ name: 'teste1', data: new Date()}]
 
 app.get('/',function(req,res) {
   res.render('index', {lstTestes: lstTestes, teste2:'Luiz'});
 });
 
-
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Server running at http://localhost:${process.env.PORT || 8080}`)
+  console.log('To shutdown the server: ctrl + c')
+})
